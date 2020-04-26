108b4bfb57f7c9aaf457a8451ca87298aff38e5f
Christopher Melby <melby.christopher@gmail.com>
2020-04-26 16:06:50 -0700

    adding main container and footer

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
-// import MainContent from "./components/MaintContent/MainContent"
-// import Footer from "./components/Footer/Footer"
+import MainContent from './components/MaintContent/MainContent';
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <div>
       <Navbar />
+      <MainContent />
+      <Footer />
     </div>
   
   );
---
+++ todo/src/components/MaintContent/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./mainContent.css";
+
+
+function MainContent() {
+
+    return (
+        <div className="container">
+            <h1>Main content goes here</h1>
+        </div>
+    )
+}
+
+export default MainContent;
